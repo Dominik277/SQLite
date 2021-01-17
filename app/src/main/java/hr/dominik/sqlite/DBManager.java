7930eb57ced475378e541cf89a0d734a431cee2e
Dominik277 <dominikdoric277@gmail.com>
2021-01-17 23:45:00 +0100

    Dodane jos metoda fetch(), update() i delete().

--- app/src/main/java/hr/dominik/sqlite/DBManager.java
+++ app/src/main/java/hr/dominik/sqlite/DBManager.java
@@ -2,6 +2,7 @@ package hr.dominik.sqlite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -30,6 +31,34 @@ public class DBManager {
         contentValues.put(DatabaseHelper.SUBJECT,name);
         contentValues.put(DatabaseHelper.DESC,desc);
         database.insert(DatabaseHelper.TABLE_NAME,null,contentValues);
+    }
+
+    public Cursor fetch(){
+        String[] columns = new String[]{
+                DatabaseHelper._ID,
+                DatabaseHelper.SUBJECT,
+                DatabaseHelper.DESC};
+        Cursor cursor = database.query(DatabaseHelper.TABLE_NAME,
+                columns,null,null,null,null,null);
+        if (cursor != null){
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public int update(long _id,String name,String desc){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseHelper.SUBJECT,name);
+        contentValues.put(DatabaseHelper.DESC,desc);
+        int i = database.update(DatabaseHelper.TABLE_NAME,
+                contentValues,DatabaseHelper._ID + " = "
+                        + _id, null);
+        return i;
+    }
+
+    public void delete(long _id){
+        database.delete(DatabaseHelper.TABLE_NAME,
+                DatabaseHelper._ID + "=" + _id,null);
     }
 
 }
