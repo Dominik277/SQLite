1ca0ddec4bb0cd3b441351b059c2ff5a61499156
Dominik277 <dominikdoric277@gmail.com>
2021-01-19 17:45:15 +0100

    Kreirana klasa DBManager koja ce imati metode za ubacivanje podataka u bazu,dobivanja podataka iz baze,azuriranje podataka u bazi i brisanja podataka iz baze.

---
+++ app/src/main/java/hr/dominik/sqlite/DBManager.java
@@ -0,0 +1,30 @@
+package hr.dominik.sqlite;
+
+import android.content.Context;
+import android.database.Cursor;
+
+public class DBManager {
+
+    private Context context;
+
+    public DBManager(Context context){
+        this.context = context;
+    }
+
+    public void insertBook(String name){
+
+    }
+
+    public Cursor getBook(){
+
+    }
+
+    public int updateRow(int id,String name){
+
+    }
+
+    public void delete(int id){
+
+    }
+
+}
