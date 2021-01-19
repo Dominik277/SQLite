09f0a46c2b92d0e40d8d7f559adf97b7edde0e8c
Dominik277 <dominikdoric277@gmail.com>
2021-01-19 16:34:13 +0100

    Kreirana SQLite helper klasa i aktivnost koja ce biti prikazana kada se klikne na neki item u listView-u.

---
+++ app/src/main/java/hr/dominik/sqlite/DatabaseHelper.java
@@ -0,0 +1,23 @@
+package hr.dominik.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public DatabaseHelper(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
