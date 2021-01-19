141821cc0966d3201963f2580cbfe1ffb3dc38c0
Dominik277 <dominikdoric277@gmail.com>
2021-01-19 17:33:57 +0100

    Kreirana tablica unutar baze podataka.

--- app/src/main/java/hr/dominik/sqlite/DatabaseHelper.java
+++ app/src/main/java/hr/dominik/sqlite/DatabaseHelper.java
@@ -6,18 +6,28 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "library";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME = "books";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
 
     public DatabaseHelper(Context context) {
-        super(context);
+        super(context,DB_NAME,null,DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + KEY_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_NAME + "TEXT"
+                + ")";
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
